2c971710a479c35eabb915c5f02907a0bbf31bf8
SaifRashid <42101711+SaifRashid@users.noreply.github.com>
2024-02-24 12:50:24 -0500

    Made Layout

---
+++ app/src/main/java/com/example/animalratingapp/AnimalRatingActivity.kt
@@ -0,0 +1,11 @@
+package com.example.animalratingapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AnimalRatingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_animal_rating)
+    }
+}
--- app/src/main/java/com/example/animalratingapp/MainActivity.kt
+++ app/src/main/java/com/example/animalratingapp/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.animalratingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val animalList = listOf("Dog", "Cat", "Bear", "Rabbit")
+        val animalAdapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, animalList)
+        val animalListView = findViewById<ListView>(R.id.listView_animal)
+        animalListView.adapter = animalAdapter
     }
 }
