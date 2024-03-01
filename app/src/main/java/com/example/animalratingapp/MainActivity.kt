20f42f50f11db59b6f3be0f1a09902e356ffe769
SaifRashid <42101711+SaifRashid@users.noreply.github.com>
2024-03-01 00:01:33 -0500

    OnStart

--- app/src/main/java/com/example/animalratingapp/MainActivity.kt
+++ app/src/main/java/com/example/animalratingapp/MainActivity.kt
@@ -6,13 +6,29 @@ import android.widget.ArrayAdapter
 import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var animalList: ArrayList<String>
+    private lateinit var animalAdapter: ArrayAdapter<String>
+    private lateinit var animalListView: ListView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val animalList = arrayListOf("Dog", "Cat", "Bear", "Rabbit")
-        val animalAdapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, animalList)
-        val animalListView = findViewById<ListView>(R.id.listView_animal)
+        // Initialize the list, adapter, and listView
+        animalList = arrayListOf("Dog", "Cat", "Bear", "Rabbit")
+        animalAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, animalList)
+        animalListView = findViewById(R.id.listView_animal)
         animalListView.adapter = animalAdapter
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val sharedPreference = getPreferences(MODE_PRIVATE)
+
+        val dictionary = sharedPreference
+
+        //
+        animalAdapter.notifyDataSetChanged()
     }
 }
