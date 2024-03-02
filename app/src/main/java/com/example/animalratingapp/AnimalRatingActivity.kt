af939fc7f7076c599b84fc233e3bb79bb8f4b97f
SaifRashid <42101711+SaifRashid@users.noreply.github.com>
2024-03-01 19:37:06 -0500

    OnStart

--- app/src/main/java/com/example/animalratingapp/AnimalRatingActivity.kt
+++ app/src/main/java/com/example/animalratingapp/AnimalRatingActivity.kt
@@ -2,10 +2,44 @@ package com.example.animalratingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.ImageView
+import android.widget.RatingBar
 
 class AnimalRatingActivity : AppCompatActivity() {
+    private lateinit var animalName: String
+    private lateinit var ratingBar: RatingBar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_animal_rating)
+
+        ratingBar = findViewById(R.id.ratingBar)
+        animalName = intent.getStringExtra("animalName")!!
+        val image = findViewById<ImageView>(R.id.image_animal)
+
+        val id = when (animalName) {
+            "Dog" -> R.drawable.dog
+            "Cat" -> R.drawable.cat
+            "Bear" -> R.drawable.bear
+            else -> R.drawable.rabbit
+        }
+        image.setImageResource(id)
+
+        val sharedPreference = getPreferences(MODE_PRIVATE)
+        val rating = sharedPreference.getFloat(animalName, -1F)
+        if (rating != -1F) {
+            ratingBar.rating = rating
+        } else {
+            ratingBar.rating = 0F
+        }
+    }
+
+    fun saveRating(view: View) {
+        val sharedPreference = getPreferences(MODE_PRIVATE)
+        val editor = sharedPreference.edit()
+        editor.putFloat(animalName, ratingBar.rating)
+        editor.apply()
+        Log.i("AnimalRatingActivity", "$animalName : ${ratingBar.rating}")
     }
 }
--- app/src/main/java/com/example/animalratingapp/MainActivity.kt
+++ app/src/main/java/com/example/animalratingapp/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.animalratingapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.ArrayAdapter
 import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var animalList: ArrayList<String>
+    private lateinit var list: List<String>
+    private lateinit var animalList: MutableList<String>
     private lateinit var animalAdapter: ArrayAdapter<String>
     private lateinit var animalListView: ListView
 
@@ -14,11 +17,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Initialize the list, adapter, and listView
-        animalList = arrayListOf("Dog", "Cat", "Bear", "Rabbit")
+        list = listOf("Dog", "Cat", "Bear", "Rabbit")
+        animalList = mutableListOf("Dog", "Cat", "Bear", "Rabbit")
         animalAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, animalList)
         animalListView = findViewById(R.id.listView_animal)
         animalListView.adapter = animalAdapter
+
+        animalListView.setOnItemClickListener { parent, view, position, id ->
+            val myIntent = Intent(this, AnimalRatingActivity::class.java)
+            myIntent.putExtra("animalName", list[position])
+            startActivity(myIntent)
+        }
     }
 
     override fun onStart() {
@@ -26,9 +35,16 @@ class MainActivity : AppCompatActivity() {
 
         val sharedPreference = getPreferences(MODE_PRIVATE)
 
-        val dictionary = sharedPreference
-
-        //
+        for ( (index, animalName) in list.withIndex()) {
+            val rating = sharedPreference.getFloat(animalName, -1F)
+            Log.i("MainActivity", "$animalName : $rating")
+            if (rating != -1F) {
+                animalList[index] = "$animalName -- Rating: $rating"
+            } else {
+                animalList[index] = "$animalName $rating"
+            }
+        }
         animalAdapter.notifyDataSetChanged()
     }
+
 }
